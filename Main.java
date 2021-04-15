1af6eeb5ef677381080d7c48fa7d9cbc946301be
Marko Andrushchenko <markello123456789101@gmail.com>
2021-04-15 15:46:59 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,12 +4,14 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) {
-//    Uncomment below to use Array method and comment LinkedList method
+// 1    Uncomment group of code below to use Array method and comment LinkedList method
+        
 //        StudentHelper studentHelper = new StudentHelper();
 //        Student[] array = studentHelper.createArrayOfStudents(2);
 //        studentHelper.printArrayOfStudents(array);
 
-//    Uncomment below to use LinkedList method and comment Array method
+// 2    Uncomment group of code below to use LinkedList method and comment Array method
+        
         StudentHelperList studentHelperList = new StudentHelperList();
         LinkedList<Student> list = studentHelperList.createListOfStudents(2);
         studentHelperList.printListOfStudents(list);
